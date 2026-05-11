f1b8a5b91c35cb932362f9007fa5b4255d1f792a
HashigoHashi <ddaigotkh@gmail.com>
2026-05-11 22:58:03 +0900

    eh02

--- eh02/echo/main.go
+++ eh02/echo/main.go
@@ -12,5 +12,32 @@ import (
 )
 
 func main() {
-	// ここにコードを書く
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("request received")
+		fmt.Fprintf(w, "Hello, Container!")
+	})
+
+	log.Println("Start server")
+	server := &http.Server{
+		Addr:    ":8080",
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown(): %s", err)
+	}
+	log.Println("Server terminated")
+	
 }
